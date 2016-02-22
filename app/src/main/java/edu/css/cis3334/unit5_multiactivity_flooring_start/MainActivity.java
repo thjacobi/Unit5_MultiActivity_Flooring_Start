098c65179b94a8e482b4fa34cb83ca4019c84cb7
thjacobi <tjacobi@css.edu>
2016-02-22 00:31:51 -0600

    Removed unused imports

--- app/src/main/java/edu/css/cis3334/unit5_multiactivity_flooring_start/MainActivity.java
+++ app/src/main/java/edu/css/cis3334/unit5_multiactivity_flooring_start/MainActivity.java
@@ -1,15 +1,19 @@
 package edu.css.cis3334.unit5_multiactivity_flooring_start;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
 
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
@@ -36,4 +40,30 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void calculateArea(View v) {
+
+        //Find the input values
+        final EditText inputWidth = (EditText) findViewById(R.id.etWidth);
+        final EditText inputLength = (EditText) findViewById(R.id.etLength);
+       // Button btnNextScreen = (Button) findViewById(R.id.btnShowResults);
+        final String widthString = inputWidth.getText().toString();
+        final String lengthString = inputLength.getText().toString();
+
+        //Cast the input to float when input is not empty
+        float width = Float.valueOf(widthString);
+        float length = Float.valueOf(lengthString);
+
+        //Calculate flooring area
+        float floorArea = width * length;
+
+        //Create output string
+        String flooringNeed = ("Width is " + width + " and Length is " + length + "\n\nThe total flooring needed is " + floorArea);
+
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        intent.putExtra("area", flooringNeed);
+        startActivity(intent);
+    }
+
+
 }
